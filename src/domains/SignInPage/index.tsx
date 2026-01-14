42a696cef4861a56b2a28323e99d1f7edef33b0a
dawoonkim <hec8897@naver.com>
2026-01-14 15:03:59 +0900

    feat: SignInPage 임시 네비게이션 함수 추가
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/domains/SignInPage/index.tsx
+++ src/domains/SignInPage/index.tsx
@@ -1,6 +1,13 @@
 import { Button, Input } from "@/shared";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "@/routes/paths";
 
 export function SignInPage() {
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    navigate(PATHS.HOME);
+  };
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-4 gap-8">
       <h1 className="text-2xl font-bold">로그인</h1>
@@ -8,7 +15,7 @@ export function SignInPage() {
         <Input label="이메일" placeholder="email@example.com" />
         <Input label="비밀번호" placeholder="비밀번호" type="password" />
       </div>
-      <Button disabled>로그인</Button>
+      <Button onClick={onSubmit}>로그인</Button>
     </div>
   );
 }
