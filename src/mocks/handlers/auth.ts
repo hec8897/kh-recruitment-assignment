56ba430a73fd183dd362b7da13454640a8aebea6
dawoonkim <hec8897@naver.com>
2026-01-14 12:03:32 +0900

    feat: MSW user 핸들러 추가 - GET /api/refresh 토큰 갱신
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -1,7 +1,8 @@
 import { http, HttpResponse } from "msw";
-import type { SignInRequest } from "@/types";
+import type { SignInRequest, AuthTokens } from "@/types";
 
 export const authHandlers = [
+  // POST /api/sign-in : 로그인 요청 처리
   http.post("/api/sign-in", async ({ request }) => {
     const { email, password } = (await request.json()) as SignInRequest;
     if (email === "test@example.com" && password === "password123") {
@@ -18,6 +19,36 @@ export const authHandlers = [
       {
         status: 401,
       }
+    );
+  }),
+
+  // POST /api/refresh 토큰 갱신
+  http.post("/api/refresh", async ({ request }) => {
+    const { refreshToken } = (await request.json()) as AuthTokens;
+    if (!refreshToken) {
+      return HttpResponse.json(
+        {
+          errorMessage: "리프레시 토큰이 필요합니다.",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    if (
+      refreshToken === "mock-refresh-token" ||
+      refreshToken === "new-mock-refresh-token" // 갱신된 새로운 토큰
+    ) {
+      return HttpResponse.json({
+        accessToken: "new-mock-access-token",
+        refreshToken: "new-mock-refresh-token",
+      });
+    }
+
+    return HttpResponse.json(
+      { errorMessage: "유효하지 않은 토큰입니다." },
+      { status: 401 }
     );
   }),
 ];
