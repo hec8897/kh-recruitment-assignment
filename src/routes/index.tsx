086fa290c936d49070f4a10308030ae9d352972c
dawoonkim <hec8897@naver.com>
2026-01-14 13:53:10 +0900

    feat: 라우터 구성 완료 - 경로별 라우터 설정 - Outlet 으로 인증 필요 라우트 그룹화 ProtectRoute 대체 예정
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,34 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import { PATHS } from "./paths";
 
 export const router = createBrowserRouter([
   {
-    path: PATHS.HOME,
-    element: <div>대시보드</div>,
+    path: PATHS.SIGN_IN,
+    element: <div>로그인</div>,
+  },
+  {
+    element: <Outlet />,
+    children: [
+      {
+        path: PATHS.HOME,
+        element: <div>대시보드</div>,
+      },
+      {
+        path: PATHS.TASK,
+        element: <div>할 일 목록</div>,
+      },
+      {
+        path: PATHS.TASK_DETAIL,
+        element: <div>할 일 상세</div>,
+      },
+      {
+        path: PATHS.USER,
+        element: <div>회원정보</div>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
   },
 ]);
