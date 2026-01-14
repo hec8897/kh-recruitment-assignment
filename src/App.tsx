9d6e2d40895df7561b22500fb4fa6184ef3dd38a
dawoonkim <hec8897@naver.com>
2026-01-14 22:54:17 +0900

    feat: Axios Request 인터셉터 설정 - api 인스턴스 생성 - accessToken 자동 첨부
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
 
 export default function App() {
-  useEffect(() => {
-    // 로그인 API 테스트
-    fetch("/api/sign-in", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: "test@example.com",
-        password: "password123",
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log("✅ 성공:", data))
-      .catch((err) => console.error("❌ 실패:", err));
-  }, []);
   return (
     <>
       <RouterProvider router={router} />
