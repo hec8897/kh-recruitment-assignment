6453d14a85b6aa221916ee7b58173728227a9dec
dawoonkim <hec8897@naver.com>
2026-01-14 23:33:08 +0900

    tanstack query 를 사용하기 위한 설정 추가
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
