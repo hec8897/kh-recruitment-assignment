cef20efe13617020f5405eece0a19ba31279049a
dawoonkim <hec8897@naver.com>
2026-01-15 10:17:33 +0900

    refactor: 대시보드 페이지 로딩 상태 처리 및 카드 라벨 수정
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/domains/home/pages/DashBoardPage/index.tsx
+++ src/domains/home/pages/DashBoardPage/index.tsx
@@ -2,12 +2,14 @@ import { DashBoardCard } from "../../components/DashBoardCard";
 import { useDashboard } from "../../hooks/useDashboard";
 
 export function DashBoardPage() {
-  const { data: dashboard } = useDashboard();
+  const { data: dashboard, isLoading } = useDashboard();
+
+  if (isLoading) return <div>Loading...</div>;
   return (
     <div>
       <div className="border border-gray-200 rounded-xl flex px-2 py-4">
         <DashBoardCard
-          title="일"
+          title="전체"
           count={dashboard?.numOfDoneTask || 0}
           isBorderRight
         />
@@ -16,7 +18,7 @@ export function DashBoardPage() {
           count={dashboard?.numOfRestTask || 0}
           isBorderRight
         />
-        <DashBoardCard title="한 일" count={dashboard?.numOfTask || 0} />
+        <DashBoardCard title="완료한 일" count={dashboard?.numOfTask || 0} />
       </div>
     </div>
   );
