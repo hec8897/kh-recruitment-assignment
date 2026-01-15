bba551ef64bba9ac381f7c8dbc9ebe0bc766542a
dawoonkim <hec8897@naver.com>
2026-01-15 11:38:21 +0900

    feat: 유저 API 연동 및 로그인 상태 연동 - useUser 훅에서 로그인 상태 반환 추가 - UserPage 로그아웃 기능 추가 - GNB 에서 useUser 활용 로그인 상태 표시
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/domains/user/pages/UserPage/index.tsx
+++ src/domains/user/pages/UserPage/index.tsx
@@ -1,13 +1,29 @@
+import { Button } from "@/shared";
+
+import { useNavigate } from "react-router-dom";
 import { useUser } from "../../hooks/useUser";
+
+import { PATHS } from "@/routes/paths";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/lib/constants";
 
 export function UserPage() {
   const { data: user, isLoading } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem(ACCESS_TOKEN_KEY);
+    localStorage.removeItem(REFRESH_TOKEN_KEY);
+    navigate(PATHS.SIGN_IN);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-2">{user?.name}</h2>
-      <p>{user?.memo}</p>
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold mb-2">{user?.name}</h2>
+        <p>{user?.memo}</p>
+      </div>
+      <Button onClick={handleLogout}>로그아웃</Button>
     </div>
   );
 }
